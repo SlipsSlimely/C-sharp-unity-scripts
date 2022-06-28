a6f15e65dc972a41074bb1b75df49f1a9ee022d4
SlipsSlimely <107329067+SlipsSlimely@users.noreply.github.com>
2022-06-28 14:43:35 -0400

    Add files via upload
    Files from a roll a ball game I made in my classes

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public AudioClip winnoise;
+
+    AudioSource audioSource;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if ( other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 50)
+        {
+            winText.text = "You Win!";
+            audioSource.Play();
+        }
+    }
+}
