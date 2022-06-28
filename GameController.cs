d197b3d69ccdad3faeb2f2e8e43363dfa0e0979e
SlipsSlimely <107329067+SlipsSlimely@users.noreply.github.com>
2022-06-28 14:44:13 -0400

    Add files via upload
    Files from a galaga style game I made in my classes

--- GameController.cs
+++ GameController.cs
@@ -1,190 +1,81 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public Text restartText;
-    //public Text coinCounter;
-    public Text timeText;
-    public Image scoreText;
-    public InputField nameInput;
-    public Button submitButton;
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
 
-
-    private bool runTimer;
-    private bool restart;
+    public Text scoreText;
+    
+    public Text gameOverText;
+    private int score;
     private bool gameOver;
+    private bool restart;
 
-    private float currentTime;
-
-    // Variables that will store the player and the player controller script, which will be used for things such as finding the player's current health.
-    private GameObject player;
-    private PlayerController playerController;
-
-    // Variables for calculating the final score
-    private int coinCount;
-    private int timeScore;
-    private int finalScore;
-
-    // Get the canvas to access the pause script there.
-    public GameObject canvas;
-    private bool isPaused;
-
-    // Stops all audio on death
-    public GameObject camera;
-    private AudioSource musicSource;
-
-    // Valariable containing the name InputField
-    public InputField inputField;
-
-    // Variables for getting a new score and adding it to the array of scores
-    private static int newScore = 0;
-    private static string newName = "";
-
-    // Start is called before the first frame update
-    void Start()
+    void Start ()
     {
-        AudioListener.pause = false;
-        restartText.gameObject.SetActive(false);
-        scoreText.gameObject.SetActive(false);
-        nameInput.gameObject.SetActive(false);
-        submitButton.gameObject.SetActive(false);
-
-        //coinCounter.text = "";
-
-        coinCount = 0;
-        currentTime = 0;
-        runTimer = true;
         restart = false;
         gameOver = false;
-
-        player = GameObject.FindGameObjectWithTag("Player");
-        playerController = player.GetComponent<PlayerController>();
-
-        musicSource = camera.GetComponent<AudioSource>();
+       
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
     }
 
     void Update()
     {
-        isPaused = canvas.GetComponent<PauseMenu>().paused;
-
-        if (restart && !isPaused)
+        if (restart)
         {
-            restartText.gameObject.SetActive(true);
-            scoreText.gameObject.SetActive(true);
-            nameInput.gameObject.SetActive(false);
-            submitButton.gameObject.SetActive(false);
-
-            if (Input.GetKeyDown(KeyCode.R) && !isPaused)
+            if(Input.GetKeyDown(KeyCode.R))
             {
-                //this line of text no longer works but I have an override in so it should
                 Application.LoadLevel(Application.loadedLevel);
-                Time.timeScale = 1;
-
             }
         }
-        else if (!isPaused && gameOver) 
-        {
-            scoreText.gameObject.SetActive(true);
-            nameInput.gameObject.SetActive(true);
-            submitButton.gameObject.SetActive(true);
-        }
-        else 
-        {
-            restartText.gameObject.SetActive(false);
-            scoreText.gameObject.SetActive(false);
-            nameInput.gameObject.SetActive(false);
-            submitButton.gameObject.SetActive(false);
-        }
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    IEnumerator SpawnWaves()
     {
-        if (playerController.PlayerHealth <= 0) 
+        yield return new WaitForSeconds(startWait);
+        while (true)
         {
-            StartCoroutine(destroyPlayer(2));
-        }
-
-        if (runTimer) 
-        {
-            currentTime += Time.deltaTime;
-            DisplayTime(currentTime);
-        }
-
-
-    }
-
-    public void GameOver()
-    {
-        runTimer = false;
-
-        player.GetComponent<Collider>().enabled = false;
-
-        AudioListener.pause = true;
-
-        //Gonna have it so the final score also posts in the restart box for right now
-        timeScore = Mathf.FloorToInt(currentTime / 10);
-        restartText.text = "Game Over! (Press R to Restart)";
-        gameOver = true;
-
-        // Calculate and set final score
-        coinCount = playerController.coinCount;
-        finalScore = (timeScore + coinCount);
-        scoreText.GetComponentInChildren<Text>().text = "Score: " + finalScore;
-
-        
-    }
-
-    void DisplayTime(float time) 
-    {
-        
-        int hours = Mathf.FloorToInt(time / 3600);
-        int minutes = Mathf.FloorToInt(time / 60);
-        int seconds = Mathf.FloorToInt(time % 60);
-        int milliSeconds = (int)((time * 1000) % 1000);
-        timeText.text = string.Format("{0:00}:{1:00}:{2:00}:{3:000}", hours, minutes, seconds, milliSeconds);
-    }
-
-    // function for getting the players score and adding it to the leaderboard
-    public void getScore()
-    {
-        newName = inputField.text;
-        newScore = finalScore;
-
-        string oldName;
-        int oldScore;
-
-        for (int i = 0; i < 7; i++)
-        {
-            oldName = PlayerPrefs.GetString("Save_" + i + "_Name");
-            oldScore = PlayerPrefs.GetInt("Save_" + i + "_Score");
-
-            if (newScore > oldScore)
+            for (int i = 0; i < hazardCount; i++)
             {
-                PlayerPrefs.SetString("Save_" + i + "_Name", newName);
-                PlayerPrefs.SetInt("Save_" + i + "_Score", newScore);
-                PlayerPrefs.Save();
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
 
-                Debug.Log(PlayerPrefs.GetString("Save_" + i + "_Name"));
-                Debug.Log(PlayerPrefs.GetInt("Save_" + i + "_Score"));
-
-                newName = oldName;
-                newScore = oldScore;
+            if (gameOver)
+            {
+                gameOverText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
             }
         }
-
-        restart = true;
     }
 
-    IEnumerator destroyPlayer(int seconds)
+    public void AddScore (int newScoreValue)
     {
-        GameOver();
-        yield return new WaitForSeconds(seconds);
-        Destroy(player);
-        Time.timeScale = 0;
-
+        score += newScoreValue;
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
     }
 }
--- PlayerController.cs
+++ PlayerController.cs
@@ -1,174 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMax, zMin;
+
+}
+
 
 public class PlayerController : MonoBehaviour
 {
-    public int coinCount = 0;
-    public int PlayerHealth = 1;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
 
-    public Text CoinCounter;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
 
-    public float jumpForce;
-
-    // Used for jumping for now, but may have other uses later
-    private Rigidbody rb;
-    private bool jumpRequest;
-    private int jumpCount;
-
-    private GameObject shield;
-    public GameObject coin;
-    private bool isInvincible;
-
-    private AudioSource audioSource;
-    public AudioClip coinNoise;
-    public AudioSource powerupPlayer;
-    public AudioClip jumpNoise;
-    public AudioClip powerNoise;
-    public float volume = .5f;
-    //public PauseMenu script;
-    //private Collider col;
-
-    //public Animation jumpAnimation;
-
-    // Get the canvas to access the pause script there.
-    public GameObject canvas;
-    private bool isPaused;
-
-    // Variable to hold the Animor component
-    private Animator anim;
-    int jumpHash;
-    int deathHash;
-
-    // Start is called before the first frame update
-    void Start()
+    private float nextFire;
+    void Update ()
     {
-        anim = GetComponent<Animator>();
-        jumpHash = Animator.StringToHash("Jump");
-        deathHash = Animator.StringToHash("Death");
-
-        isPaused = false;
-        rb = this.GetComponent<Rigidbody>();
-        jumpRequest = false;
-        jumpCount = 0;
-
-        shield = gameObject.transform.Find("Shield").gameObject;
-        shield.SetActive(false);
-        // May be used to pause and play footsteps when jumping
-        audioSource = GetComponent<AudioSource>();
-
-        coinCount = 0;
-        UpdateScore();
-
+        if (Input.GetButton("Fire1")&& Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            //GameObject clone = 
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation); //as GameObject;
+            GetComponent<AudioSource>().Play();
+        }
+        
     }
 
-    // Update is called once per frame
-    [System.Obsolete]
-    void Update()
+    void FixedUpdate ()
     {
-        isPaused = canvas.GetComponent<PauseMenu>().paused;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        // Jump
-        if (!isPaused && Input.GetKeyDown(KeyCode.Space) && (IsGrounded() || jumpCount <= 1) /**&& script.paused != true**/)
-        {
-            jumpRequest = true;
-        }
-        else if (!IsGrounded()) 
-        {
-            audioSource.Pause();
-        }
-        else
-        {
-            audioSource.UnPause();
-        }
-    }
+        Vector3 movement = new Vector3(moveHorizontal, 0.0F, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * speed;
 
-    void FixedUpdate()
-    {
-        if (jumpRequest) 
-        {
-            jumpCount++;
+        GetComponent<Rigidbody>().position = new Vector3
+        (
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0F,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+        );
 
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0F, 0.0F, GetComponent<Rigidbody>().velocity.x * -tilt);
 
-            // handle the jump
-            powerupPlayer.PlayOneShot(jumpNoise, .3f);
-            anim.SetTrigger(jumpHash);
-            rb.velocity = jumpForce * Vector3.up;
-            jumpRequest = false;
-        }
-    }
-
-    //this lets our skeleton collide with coins and traps
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Coin")
-        {
-            other.gameObject.SetActive(false);
-
-            //audioSource.PlayOneShot(coinNoise, volume);
-            powerupPlayer.PlayOneShot(coinNoise, 1f);
-            coinCount = coinCount + 1;
-            coin.SetActive(true);
-            UpdateScore();
-        }
-        else if (other.tag == "Trap")
-        {
-            if (isInvincible) 
-            {
-                shield.SetActive(false);
-                StartCoroutine(invincibility());
-            }
-            else 
-            {
-                PlayerHealth = PlayerHealth - 1;
-                rb.velocity = Vector3.up * 5;
-                anim.ResetTrigger(jumpHash);
-                anim.SetTrigger(deathHash);
-            }
-        }
-        else if (other.tag == "Health-up")
-        {
-            if (PlayerHealth == 1)
-            {
-                powerupPlayer.PlayOneShot(powerNoise, volume);
-                isInvincible = true;
-                other.gameObject.SetActive(false);
-                shield.SetActive(true);
-            }
-        }
-        else if (other.tag == "Money up")
-        {
-            audioSource.PlayOneShot(coinNoise, volume);
-            audioSource.PlayOneShot(coinNoise, volume);
-            coinCount = coinCount + 10;
-        }
-    }
-    void UpdateScore()
-    {
-        coin.SetActive(false);
-        CoinCounter.text = "Money Count: " + coinCount.ToString();
-    }
-
-    private bool IsGrounded()
-    {
-        RaycastHit hit;
-        float distance = 0.15f;
-        Vector3 dir = new Vector3(0, -1);
-
-        if (Physics.Raycast(transform.position, dir, out hit, distance)) 
-        {
-            jumpCount = 0;
-            return true;
-        }
-
-        else
-            return false;
-    }
-
-    IEnumerator invincibility() 
-    {
-        yield return new WaitForSeconds(1);
-        isInvincible = false;
     }
 }
