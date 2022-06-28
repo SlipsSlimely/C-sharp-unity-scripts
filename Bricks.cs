ccdc6f35998c0c175b276bdfc4d7ee3d213d6082
SlipsSlimely <107329067+SlipsSlimely@users.noreply.github.com>
2022-06-28 14:40:18 -0400

    Add files via upload
    C# files from a breakout game I made in my classes

---
+++ Bricks.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bricks : MonoBehaviour
+{
+    public GameObject brickParticle;
+
+    public AudioSource breakNoise;
+
+
+    void OnCollisionEnter (Collision other)
+    {
+
+        Instantiate(brickParticle, transform.position, Quaternion.identity);
+        breakNoise.Play();
+        GM.instance.DestroyBrick();
+        Destroy(gameObject);
+
+        
+    }
+
+}
---
+++ DeadZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+
+    public AudioSource waterNoise;
+    void OnTriggerEnter (Collider col)
+    {
+        waterNoise.Play();
+        GM.instance.LoseLife();
+    }
+}
